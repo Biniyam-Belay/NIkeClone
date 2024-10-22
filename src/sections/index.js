686545317ecbb07b6430d24e074f2effda1eb9d6
Biniyam Belay <binibelaygom@gmail.com>
2024-10-22 17:37:24 +0300

    second commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import Specialoffer from "./Specialoffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    Specialoffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
